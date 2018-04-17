b4242d2834fba5c4494867ffb66fd5ea82066ab2
Joona Ikonen <joona.ikonen@metropolia.fi>
2018-04-17 11:03:49 +0300

    testing testing

---
+++ src/test/java/org/gradle/demo/GreeterTest.java
@@ -0,0 +1,16 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GreeterTest {
+
+    @Test
+    public void dummyUnitTest() {
+        assertEquals(0, 1);
+    }
+    @Test
+    public void dummyUnitTest2() {
+        assertEquals(1, 1);
+    }
+
+}
