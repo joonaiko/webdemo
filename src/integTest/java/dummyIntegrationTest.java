ccf61daed65044aacffdd33da0fb5bbd7f355c06
Joona Ikonen <joona.ikonen@metropolia.fi>
2018-04-17 11:53:25 +0300

    integTesting juttui

---
+++ src/integTest/java/dummyIntegrationTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+public class dummyIntegrationTest{
+
+    @Test
+    public void dummyTest() {
+        assertEquals(1, 1);
+    }
+
+}
